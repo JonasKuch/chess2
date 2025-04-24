50f5bbbc1f251982c98eb2653bdaaabf67e8a31e
Jonas Kuch <jonas.a.kuch@gmail.com>
2025-04-24 13:02:10 +0200

    Window and DrawBoard class added

--- src/chess2/gui/gui.py
+++ src/chess2/gui/gui.py
@@ -2,9 +2,33 @@ import pygame
 
 
 
-class GUI:
-    def __init__(self, board, width = 10, height = 10):
-        self.board = board
+class Window():
+    def __init__(self, width, height):
+        pygame.init()
         self.width = width
         self.height = height
-        self.square_side = (self.width - 0.05 * self.width) / 8
+        self.screen = pygame.display.set_mode(self.width, self.height)
+
+    
+    def fill(self, color):
+        self.screen.fill(color)
+
+
+    def update(self):
+        pygame.display.flip()
+
+
+
+class DrawBoard():
+    def __init__(self, surface, colors=("white", "black")):
+        self.surface = surface
+        self.color_white, self.color_black = colors
+        self.tiles_width = 0.95*self.surface.width / 8
+
+    def draw(self):
+        self.surface.fill(self.color_white)
+        for x in range(8):
+            for y in range(8):
+                rect = pygame.Rect()
+                pygame.draw.rect(self.surface, self.color_black, rect)
+
