2d13eba7a1480f33286cd2cae3421ecfc33b6fd7
Jonas Kuch <jonas.a.kuch@gmail.com>
2025-04-26 01:39:06 +0200

    transparent circles for valid moves added

--- src/chess2/gui/pieces_renderer.py
+++ src/chess2/gui/pieces_renderer.py
@@ -26,10 +26,24 @@ class PiecesRenderer():
 
 
     def draw_legal_moves(self, piece, side):
-        img_path = f"{self.pieces_dir}/dot.png"
-        img = pygame.transform.scale(pygame.image.load(img_path), (self.square_width, self.square_width))
+        offset = self.square_width/2
+        radius = self.square_width * 0.2
+        alpha = 100  # 75% transparent
+
+        # color.a = 128
         legal_moves = piece.get_legal_moves()
         for move in legal_moves:
             x, y = move
-            pos = ( x*self.square_width, (7-y)*self.square_width ) if side == Color.WHITE else ( (7-x)*self.square_width, y*self.square_width ) 
-            self.surface.blit(img, pos)
+            pos = ( x*self.square_width+offset, (7-y)*self.square_width+offset) if side == Color.WHITE else ( (7-x)*self.square_width+offset, y*self.square_width+offset ) 
+
+            # Create a temporary surface for the transparent circle
+            circle_surf = pygame.Surface((radius * 2, radius * 2), pygame.SRCALPHA)
+            circle_color = (205, 0, 0, alpha)  # RGBA
+
+            # Draw the transparent circle onto the temporary surface
+            pygame.draw.circle(circle_surf, circle_color, (radius, radius), radius)
+
+            # Blit it to the main surface
+            self.surface.blit(circle_surf, (pos[0] - radius, pos[1] - radius))
+
+
