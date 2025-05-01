db88db8d02df9718c2adefd4856a7864925bb532
Jonas Kuch <jonas.a.kuch@gmail.com>
2025-05-01 22:16:28 +0200

    dataset started

---
+++ src/chess2/bot/dataset.py
@@ -0,0 +1,8 @@
+import torch
+from torch.utils.data import Dataset
+
+class ChessDataset(Dataset):
+    def __init__(self, transform, target_transform):
+        self.transform = transform
+        self.target_transform = target_transform
+
