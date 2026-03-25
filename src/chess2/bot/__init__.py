e812de47ac7c161c861a74270934505956f2ccd1
Jonas Kuch <jonas.a.kuch@gmail.com>
2026-03-25 19:25:45 +0100

    better file names

--- src/chess2/bot/__init__.py
+++ src/chess2/bot/__init__.py
@@ -1,4 +1,4 @@
 from chess2.bot.dataset import ChessDataset
 from chess2.bot.neural_network import NeuralNetwork
-from chess2.bot.create_trainingset import TensorProcessor
+from chess2.bot.tensor_processor import TensorProcessor
 from chess2.bot.move_generation import MoveGenerator
