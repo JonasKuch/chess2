388e99076073daaf315f3a9e05560867639cab64
Jonas Kuch <jonas.a.kuch@gmail.com>
2026-03-30 22:25:35 +0200

    random move bot added to game code (commented out)

--- src/chess2/game.py
+++ src/chess2/game.py
@@ -187,6 +187,7 @@ class Game():
             bot_side = Color.BLACK if side == Color.WHITE else Color.WHITE
             if play_bot and self.board.turn == bot_side and self.running:
                 # self.board.load_state(self.bot.stockfish_move(bot_side, self.board))
+                # self.board.load_state(self.bot.make_random_move(bot_side, self.board))
                 self.board.load_state(self.bot.model_move(bot_side, self.board))
                 self.swap_turns(turn_board)
                 self.move.cache_board_state(self.board)
